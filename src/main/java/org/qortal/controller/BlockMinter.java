6548e4c07d6864c59b712a9ec2012b700a81cc1d
CalDescent <caldescent@protonmail.com>
2021-08-07 18:09:47 +0100

    Moved all online accounts code to a new class called OnlineAccountsManager
    The Controller class was a bit crowded so it makes sense to move this to a dedicated controller. There haven't been any intentional logic changes here, to keep risk to a minimum.

--- src/main/java/org/qortal/controller/BlockMinter.java
+++ src/main/java/org/qortal/controller/BlockMinter.java
@@ -103,7 +103,7 @@ public class BlockMinter extends Thread {
 					continue;
 
 				// No online accounts? (e.g. during startup)
-				if (Controller.getInstance().getOnlineAccounts().isEmpty())
+				if (OnlineAccountsManager.getInstance().getOnlineAccounts().isEmpty())
 					continue;
 
 				List<MintingAccountData> mintingAccountsData = repository.getAccountRepository().getMintingAccounts();
@@ -413,7 +413,7 @@ public class BlockMinter extends Thread {
 			throw new DataException("Ignoring attempt to mint testing block for non-test chain!");
 
 		// Ensure mintingAccount is 'online' so blocks can be minted
-		Controller.getInstance().ensureTestingAccountsOnline(mintingAndOnlineAccounts);
+		OnlineAccountsManager.getInstance().ensureTestingAccountsOnline(mintingAndOnlineAccounts);
 
 		PrivateKeyAccount mintingAccount = mintingAndOnlineAccounts[0];
 
